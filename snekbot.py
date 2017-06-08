1a5a31767d35e4a7a0695f2075e9dd669e85ccdb
IREvil <nita_andrei97@yahoo.com>
2017-06-08 03:22:49 +0300

    Create snekbot.py

--- snekbot.py
+++ snekbot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import asyncio
 import re
 import random
+import requests
+import json
 
 bot = commands.Bot(command_prefix=['snek ', 'snekbot '], description='this is snekbot.')
 
@@ -69,6 +71,28 @@ async def snek():
     """sneksneksnek"""
     sneks = ['snek', 'hss', 'ssssnek', 'slither', 'snek sneek']
     await bot.say(random.choice(sneks))
+    
+client = discord.Client()
+user = 'CLEVERBOT.IO API USER'
+key = 'CLEVERBOT.IO API KEY'
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | '+str(len(client.servers))+' servers')
+    await client.change_presence(game=discord.Game(name='chat with me!'))
+
+@client.event
+async def on_message(message):
+    if not message.author.bot and (message.server == None or client.user in message.mentions):
+        await client.send_typing(message.channel)
+        txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
+        r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'frost', 'text':txt}).text)
+        if r['status'] == 'success':
+            await client.send_message(message.channel, r['response'] )
+
+print('Starting...')
+requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'frost'})
+client.run('DISCORD BOT TOKEN')
 
 
 bot.run(open('./token','r').read().replace('\n', ''))
